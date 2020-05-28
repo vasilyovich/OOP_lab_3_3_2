62c216072d3b2e174880eebcb81237caf1317e95
Роман Данилевич <dwordchanel@gmail.com>
2020-05-28 23:41:14 +0300

    Add project files.

---
+++ OOP_lab_3_3_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("code.js");
+
+            string code = file.ReadToEnd();
+
+            int roundCount = 0;
+            int figureCount = 0;
+
+            for (int i = 1; i < code.Length; ++i)
+            {
+                switch (code[i])
+                {
+                    case '(':
+                        ++roundCount;
+                        break;
+
+                    case ')':
+                        --roundCount;
+                        break;
+
+                    case '{':
+                        ++figureCount;
+                        break;
+
+                    case '}':
+                        --figureCount;
+                        break;
+                }
+            }
+
+            if ((roundCount == 0) && (figureCount == 0))
+            {
+                Console.WriteLine("Кiлькiсть вiдкритих та закритих дужок спiвпадає.");
+            }
+            else
+            {
+                Console.WriteLine("Кiлькiсть вiдкритих та закритих дужок не спiвпадає.");
+            }
+        }
+    }
+}
